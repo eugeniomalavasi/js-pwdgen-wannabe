9be13c61cf03a345fac7aa7bb785046d6a4a29e8
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-03-22 14:53:09 +0100

    feat: variables added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 // alert ("Ciao sono la tua prima riga di js!")
 
-// dichiarazione variabili nome cognome colore preferito
+// dichiarazione variabili prompt username 
+const userName = prompt("Inserisci il tuo nome"); // string
+console.log(userName, typeof userName);
+
+// dichiarazione variabili prompt cognome
+const surName = prompt ("Inserisci il tuo cognome"); // string
+console.log(surName, typeof surName);
+
+// dichiarazione colore preferito
+const prefClr = prompt("Inserisci il tuo colore preferito"); // string
+console.log(prefClr, typeof prefClr)
+
+// unione variabili 
